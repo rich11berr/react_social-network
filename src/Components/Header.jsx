00aa06e15461f84b949efc90d70d2de317dc91b2
rich11berr <rinocraft.tv@gmail.com>
2021-09-06 16:21:37 +0400

    samurai l11

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmcYhwCLfI3QTKtveCcdl2VZeG66f6jVUXCg&usqp=CAU"
+          alt="logo"
+        ></img>
+      </header>
+    )
+}
