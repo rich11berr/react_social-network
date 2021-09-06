2258131c03e09908f65383791fecde18ae36c90e
rich11berr <rinocraft.tv@gmail.com>
2021-09-06 18:46:28 +0400

    samurai l12

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+      <nav className="nav">
+      <ul>
+        <li>
+          <a href="#s">Profile</a>
+        </li>
+        <li>
+          <a href="#s">Messages</a>
+        </li>
+        <li>
+          <a href="#s">News</a>
+        </li>
+        <li>
+          <a href="#s">Music</a>
+        </li>
+      </ul>
+    </nav>
+    )
+}
+
+export default Nav;
