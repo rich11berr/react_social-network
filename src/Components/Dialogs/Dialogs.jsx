1e5f49d7b148df6803b7e8a6d4eb135d464d6fe7
rich11berr <rinocraft.tv@gmail.com>
2021-09-11 23:13:57 +0400

    samurai l20-21

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -3,9 +3,39 @@ import styles from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <main className={styles.main}>
-            Dialogs
-        </main>
+        <div className={styles.dialogs}>
+            <div className={styles.dialog_users}>
+                <div className={styles.dialog_user + ' ' + styles.active}>
+                    <p>Ilya</p>
+                </div>
+                <div className={styles.dialog_user}>
+                    <NavLink>Masha</NavLink>
+                </div>
+                <div className={styles.dialog_user}>
+                    <NavLink>Arina</NavLink>
+                </div>
+                <div className={styles.dialog_user}>
+                    <NavLink>Artyom</NavLink>
+                </div>
+                <div className={styles.dialog_user}>
+                    <NavLink>Keks</NavLink>
+                </div>
+                <div className={styles.dialog_user}>
+                    <NavLink>Dima</NavLink>
+                </div>
+            </div>
+            <div className={styles.dialog_messages}>
+                <div className={styles.dialog_message}>
+                    <p>Hello world</p>
+                </div>
+                <div className={styles.dialog_message}>
+                    <p>Kek</p>
+                </div>
+                <div className={styles.dialog_message}>
+                    <p>Lol</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Nav.module.css';
 
 const Nav = () => {
     return (
       <nav className={styles.nav}>
-      <ul>
-        <li>
-          <a href="/profile">Profile</a>
-        </li>
-        <li>
-          <a href="/dialogs">Dialogs</a>
-        </li>
-        <li>
-          <a href="#s">News</a>
-        </li>
-        <li>
-          <a href="#s">Music</a>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <NavLink to="/profile">Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dialogs">Dialogs</NavLink> 
+          </li>
+          <li>
+            <NavLink to="#s">News</NavLink>
+          </li>
+          <li>
+            <NavLink to="#s">Music</NavLink>
+          </li>
+        </ul>
     </nav>
     )
 }
