ccc0afc49ca2ac6dca01d8b8eb35d24469442413
rich11berr <rinocraft.tv@gmail.com>
2021-09-11 23:33:29 +0400

    fix: refactor

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,42 +1,40 @@
-import React from "react"
-import styles from "./Dialogs.module.css"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Dialogs.module.css";
+
+const DialogUser = (props) => {
+  return (
+    <div className={styles.dialog_user}>
+      <NavLink to={"/dialogs/" + props.id} activeClassName="active">
+        {props.username}
+      </NavLink>
+    </div>
+  );
+};
 
 const Dialogs = (props) => {
-    return (
-        <div className={styles.dialogs}>
-            <div className={styles.dialog_users}>
-                <div className={styles.dialog_user + ' ' + styles.active}>
-                    <p>Ilya</p>
-                </div>
-                <div className={styles.dialog_user}>
-                    <NavLink>Masha</NavLink>
-                </div>
-                <div className={styles.dialog_user}>
-                    <NavLink>Arina</NavLink>
-                </div>
-                <div className={styles.dialog_user}>
-                    <NavLink>Artyom</NavLink>
-                </div>
-                <div className={styles.dialog_user}>
-                    <NavLink>Keks</NavLink>
-                </div>
-                <div className={styles.dialog_user}>
-                    <NavLink>Dima</NavLink>
-                </div>
-            </div>
-            <div className={styles.dialog_messages}>
-                <div className={styles.dialog_message}>
-                    <p>Hello world</p>
-                </div>
-                <div className={styles.dialog_message}>
-                    <p>Kek</p>
-                </div>
-                <div className={styles.dialog_message}>
-                    <p>Lol</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles.dialog_users}>
+        <DialogUser username="username1" id="1" />
+        <DialogUser username="username2" id="2" />
+        <DialogUser username="username3" id="3" />
+        <DialogUser username="username4" id="4" />
+        <DialogUser username="username5" id="5" />
+      </div>
+      <div className={styles.dialog_messages}>
+        <div className={styles.dialog_message}>
+          <p>Hello world</p>
         </div>
-    )
-}
+        <div className={styles.dialog_message}>
+          <p>Kek</p>
+        </div>
+        <div className={styles.dialog_message}>
+          <p>Lol</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dialogs
+export default Dialogs;
