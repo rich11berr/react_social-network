2f11dcb41e30ad62371b1d5a35286c34b1d27adf
rich11berr <rinocraft.tv@gmail.com>
2021-09-15 16:21:16 +0400

    samurai l24

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,6 +2,39 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
+let userData = [
+  {
+    id: 1,
+    name: "James",
+    userImg:
+      "https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+  },
+  {
+    id: 2,
+    name: "Nika",
+    userImg:
+      "https://images.unsplash.com/photo-1619694770795-e21c58464159?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    id: 3,
+    name: "Mike",
+    userImg:
+      "https://images.unsplash.com/photo-1631242918552-292249767127?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+  },
+  {
+    id: 4,
+    name: "Lora",
+    userImg:
+      "https://images.unsplash.com/photo-1620167972495-b6b7bdf32c5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+];
+
+let messagesData = [
+  { id: 1, message: "Yo", date: "11:11" },
+  { id: 2, message: "Do this shit faster", date: "11:12" },
+  { id: 3, message: "U can do it!", date: "11:13" },
+];
+
 const DialogUser = (props) => {
   let path = "/dialogs/" + props.id;
   return (
@@ -31,29 +64,30 @@ const Dialogs = (props) => {
     <div className={styles.dialogs}>
       <div className={styles.dialog_users}>
         <DialogUser
-          username="username1"
-          id="1"
-          userImg="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          username={userData[0].name}
+          id={userData[0].id}
+          userImg={userData[0].userImg}
         />
         <DialogUser
-          username="username2"
-          id="2"
-          userImg="https://images.unsplash.com/photo-1619694770795-e21c58464159?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          username={userData[1].name}
+          id={userData[1].id}
+          userImg={userData[1].userImg}
         />
         <DialogUser
-          username="username3"
-          id="3"
-          userImg="https://images.unsplash.com/photo-1631242918552-292249767127?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+          username={userData[2].name}
+          id={userData[2].id}
+          userImg={userData[2].userImg}
         />
         <DialogUser
-          username="username4"
-          id="4"
-          userImg="https://images.unsplash.com/photo-1620167972495-b6b7bdf32c5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          username={userData[3].name}
+          id={userData[3].id}
+          userImg={userData[3].userImg}
         />
       </div>
       <div className={styles.dialog_messages}>
-        <Message messageText="Yo" />
-        <Message messageText="Do this shit faster! =)" />
+        <Message messageText={messagesData[0].message} />
+        <Message messageText={messagesData[1].message} />
+        <Message messageText={messagesData[2].message} />
       </div>
     </div>
   );
--- src/Components/Profile/Posts/Posts.jsx
+++ src/Components/Profile/Posts/Posts.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import Post from "./Post/Post";
 import styles from "./Posts.module.css";
 
+let postsData = [
+  {
+    id: 1,
+    postText: "This is easier then I thought",
+    like: "3",
+    date: "11:11",
+  },
+  { id: 2, postText: "It's my first post here)", like: "4", date: "11:12" },
+];
+
 const Posts = () => {
   return (
     <div className={styles.posts}>
@@ -10,12 +20,8 @@ const Posts = () => {
         <input placeholder="Type here..." />
         <button>send</button>
       </div>
-      <Post message="This is easier then I thought" like="3" />
-      <Post message="It's my first post here)" like="4" />
-      {/* <Post />
-            <Post />
-            <Post />
-            <Post /> */}
+      <Post message={postsData[0].postText} like={postsData[0].like} />
+      <Post message={postsData[1].postText} like={postsData[1].like} />
     </div>
   );
 };
